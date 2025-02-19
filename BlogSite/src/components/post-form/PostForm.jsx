b8d36463c5e76753a463caea85de76b8c81c2c1f
TEJASWADPILLEWAR7 <tejaswadpillewar7@gmail.com>
2025-02-19 18:12:39 +0530

    implement submit logic for updating and creating posts with image handling

--- BlogSite/src/components/post-form/PostForm.jsx
+++ BlogSite/src/components/post-form/PostForm.jsx
@@ -23,7 +23,33 @@ export default function PostForm({ post }) {
 
     const userData = useSelector((state) => state.auth.userData)
 
-    const submit = async (data) => { }
+    const submit = async (data) => {
+        if (post) {
+            const file = data.image[0] ? await appwriteSerice.uploadFile(data.image[0]) : null
+
+            if (file) {
+                await appwriteSerice.deleteFile(post.featuredImage)
+            }
+            const dbPost = await appwriteSerice.updatePost(post.$id, {
+                ...data,
+                featuredImage: file ? file.$id : undefined
+            })
+            if (dbPost) {
+                navigate(`/post/${dbPost.$id}`)
+            }
+        } else {
+            const file = await appwriteSerice.uploadFile(data.image[0])
+            if (file) {
+                const fileId = file.$id
+                data.featuredImage = fileId
+                const dbPost = await appwriteSerice.createPost({ ...data, userId: userData.$id })
+
+                if (dbPost) {
+                    navigate(`/post/${dbPost.$id}`)
+                }
+            }
+        }
+    }
 
     const slugTransform = useCallback((value) => {
         if (value && typeof value === "string") {
@@ -44,6 +70,62 @@ export default function PostForm({ post }) {
         <form onSubmit={handleSubmit(submit)}
             className="flex flex-wrap"
         >
+            <div className="w-2/3 px-2">
+                <Input
+                    label="Title"
+                    placholder="Title"
+                    className="mb-4"
+                    {...register("title", { required: true })}
+                />
+                <Input
+                    label="slug :"
+                    placholder="slug"
+                    className="mb-4"
+                    {...register("slug", { required: true })}
+                    onInput={(e) => {
+                        setValue("slug", slugTransform(e.currentTarget.value), { shouldValidate: true })
+                    }}
+
+                />
+
+                <RTE
+                    label="Content: "
+                    name="content"
+                    control={control}
+                    defaultValue={getValues("content")}
+                />
+
+            </div>
+
+            <div className="1/3 px-2">
+                <Input
+                    label="Featured Image"
+                    type="file"
+                    className="mb-4"
+                    accept="image/png, image/jpg, image/jpeg"
+                    {...register("image", { required: !post })}
+                />
+                {
+                    post && (
+                        <div className="w-full mb-4 "><img src={appwriteSerice.getFilePreview(post.featuredImage)} alt={post.title}
+                            className="rounded-lg"
+                        /></div>
+                    )
+                }
+
+                <Select
+                    options={["active", "inactive"]}
+                    label="Status"
+                    className="mb-4"
+                    {...register("status", { required: true })}
+
+                />
+                <Button
+                    type="submit"
+                    bgColor={post ? "bg-green-500" : undefined}
+                    className="w-full"
+                />{post ? "Update" : "Submit"}
+            </div>
 
         </form>
     )
